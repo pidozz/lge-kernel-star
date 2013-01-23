8f140e1bd50ea02c3eb033d14b4a3c38f9860ee4
pidozz <crs.enrico.2@gmail.com>
2013-01-23 21:04:40 +0100

    cpufreq: interactive: Boostpulse only if current freq < hispeed freq

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -839,13 +839,22 @@ static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
 	int ret;
 	unsigned long val;
 
+	struct cpufreq_interactive_cpuinfo *pcpu;
+	unsigned int cur_freq;
+	pcpu = &per_cpu(cpuinfo, 0);
+	cur_freq = pcpu->policy->cur;
+
 	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 
-	boostpulse_endtime = ktime_to_us(ktime_get()) + boostpulse_duration_val;
-	trace_cpufreq_interactive_boost("pulse");
-	cpufreq_interactive_boost();
+	if (cur_freq >= hispeed_freq)
+		return;
+	else {
+		boostpulse_endtime = ktime_to_us(ktime_get()) + boostpulse_duration_val;
+		trace_cpufreq_interactive_boost("pulse");
+		cpufreq_interactive_boost();
+	}
 	return count;
 }
 
